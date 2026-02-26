cf392d2de27b062c33cc1e292439f021cd1db561
Amasyalionur <amasyalionur@googlemail.com>
2026-02-26 11:16:19 +0000

    Entscheidung Getränk

---
+++ com/Bestellung.java
@@ -0,0 +1,51 @@
+package com;
+
+public class Bestellung {
+    
+
+    public static void main(String[] args) {
+        // Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+
+        System.out.println(food);
+        System.out.println(drink);
+
+
+ // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Herzlich Willkommen bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
+
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+
+
+
+         // Zuordnung Zahl --> Burger
+        switch (itemNumStr)
+        {
+         case "1":
+             food = "Hamburger";
+             break;
+        case "2":
+            food = "Cheeseburger";
+            break;
+        case "3":
+             food = "Chilliburger";
+             break;
+        }
+
+        System.out.println(food);
+
+                     // Entscheidung Getränk dazu?
+        System.out.println("-------------------------------");
+        System.out.println("Möchten Sie ein Getränk dazu bestellen? (j/n)");
+       flag = System.console().readLine("(j/n)? :");
+
+
+        
+    }
+}
--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,17 @@ public class App
 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       // Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+ 
+        // System.out.println(food);
+        // System.out.println(drink
+
+
+
     }
     
 }
